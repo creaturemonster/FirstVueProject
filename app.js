b32d86ea1b974e9f0eac984802a4a45607ab0d54
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-22 17:30:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+const buttonEl=document.querySelector('button');
+const inputEl=document.querySelector('input');
+const listEl=document.querySelector('ul');
 
+buttonEl.addEventListener('click');
+
+function addGoal(){
+  const enteredValue=inputEl.value;
+  const listItemEl=document.createElement('li');
+  listItemEl.textContent=enteredValue;
+  listEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener('click', addGoal);
