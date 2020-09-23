34494e4a916f72ed83d9dc646226cdfd46cd2407
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-23 16:50:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 Vue.createApp({
-  data(){
+  data() {
     return {
-       goals:[], 
-      enteredValue:''
+      goals: [],
+      enteredValue: ''
     };
-  }, 
-  methods:{
-    addGoal(){
+  },
+  methods: {
+    addGoal() {
       this.goals.push(this.enteredValue);
+      this.enteredValue = '';
     }
   }
 }).mount('#app');
-/*const buttonEl=document.querySelector('button');
-const inputEl=document.querySelector('input');
-const listEl=document.querySelector('ul');
 
-buttonEl.addEventListener('click');
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-function addGoal(){
-  const enteredValue=inputEl.value;
-  const listItemEl=document.createElement('li');
-  listItemEl.textContent=enteredValue;
-  listEl.appendChild(listItemEl);
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
 
-buttonEl.addEventListener('click', addGoal);*/
-
+// buttonEl.addEventListener('click', addGoal);
